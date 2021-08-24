819e8407d8fd6a9052a40d6c575ab08af7927cf0
Gavin Mohit <gavin.mohit@ithands.biz>
2021-08-24 19:37:29 +0530

    setup project architecture

---
+++ lib/config/app_config.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+enum Flavor { DEV, PROD }
+
+class FlavorValues {
+  FlavorValues({required this.baseUrl});
+  final String baseUrl;
+  //Add other flavor specific values, e.g database name
+}
+
+class FlavorConfig {
+  final Flavor flavor;
+  final String name;
+  final FlavorValues values;
+  static FlavorConfig? _instance;
+
+  factory FlavorConfig(
+      {@required Flavor? flavor, @required FlavorValues? values}) {
+    _instance ??=
+        FlavorConfig._internal(flavor!, enumName(flavor.toString()), values!);
+    return _instance!;
+  }
+  FlavorConfig._internal(this.flavor, this.name, this.values);
+  static FlavorConfig? get instance {
+    return _instance;
+  }
+
+  static bool isProduction() => _instance!.flavor == Flavor.PROD;
+  static bool isDevelopment() => _instance!.flavor == Flavor.DEV;
+
+  static enumName(String value) {
+    if (value == "0") {
+      return 'DEV';
+    } else {
+      return 'PROD';
+    }
+  }
+}
