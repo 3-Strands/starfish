8320580aa8243daf6f367cdae736efa2f88332dc
Caleb Kapil <caleb.kapil@ithands.net>
2021-09-13 14:47:35 +0530

    changed hex color format

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -3,25 +3,25 @@ import 'package:flutter/material.dart';
 import 'hex_color.dart';
 
 class AppColors {
-  static Color primary =Color(0xFF434141);
+  static Color primary = Color(0xFF434141);
   static Color background = Color(0xFFFFFFFF);
   static Color txtFieldBackground = Color(0xFFEFEFEF);
 
   static Color appTitle = Color(0xFF4F4F4F);
-  static Color txtFieldTextColor = HexColor('#434141');
-  static Color selectedButtonBG = HexColor('#3475F0');
-  static Color unselectedButtonBG = HexColor('#ADADAD');
+  static Color txtFieldTextColor = Color(0xFF434141);
+  static Color selectedButtonBG = Color(0xFF3475F0);
+  static Color unselectedButtonBG = Color(0xFFADADAD);
 
-  static Color materialSceenBG = HexColor('#D7E4FC');
-  static Color groupScreenBG = HexColor('#F7E8CC');
-  static Color actionScreenBG = HexColor('#E2CDE4');
+  static Color materialSceenBG = Color(0xFFD7E4FC);
+  static Color groupScreenBG = Color(0xFFF7E8CC);
+  static Color actionScreenBG = Color(0xFFE2CDE4);
 
-  static Color materialTabBarTextColor = HexColor('#3475F0');
-  static Color groupTabBarTextColor = HexColor('#D78A00');
-  static Color actionTabBarTextColor = HexColor('#800080');
+  static Color materialTabBarTextColor = Color(0xFF3475F0);
+  static Color groupTabBarTextColor = Color(0xFFD78A00);
+  static Color actionTabBarTextColor = Color(0xFF800080);
 
-  static Color completeTaskBGColor = HexColor('#6DE26B');
-  static Color assignedTaskBGColor = HexColor('#CBE8FA');
-  static Color overdueTaskBGColor = HexColor('#FF5E4D');
-  static Color notCompletedTaskBGColor = HexColor('#FFBE4A');
+  static Color completeTaskBGColor = Color(0xFF6DE26B);
+  static Color assignedTaskBGColor = Color(0xFFCBE8FA);
+  static Color overdueTaskBGColor = Color(0xFFFF5E4D);
+  static Color notCompletedTaskBGColor = Color(0xFFFFBE4A);
 }
