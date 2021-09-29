6b67f556258b10b942873f7285b25bbacbd11bf8
Gavin Mohit <gavin.mohit@ithands.biz>
2021-09-29 13:12:05 +0530

    working on action status widget

---
+++ lib/enums/action_status.dart
@@ -0,0 +1,5 @@
+class ActionStatus {
+  static String get done => "I did it";
+  static String get notdone => "I didn't do it yet";
+  static String get overdue => "Overdue";
+}
--- lib/modules/actions_view/me.dart
+++ lib/modules/actions_view/me.dart
@@ -1,5 +1,12 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:starfish/constants/app_colors.dart';
+import 'package:starfish/constants/strings.dart';
+import 'package:starfish/db/hive_action.dart';
+import 'package:starfish/enums/action_status.dart';
+import 'package:starfish/widgets/custon_icon_button.dart';
 import 'package:starfish/widgets/searchbar_widget.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -96,7 +103,97 @@ class _MeState extends State<Me> {
               print('searched value $value');
             },
           ),
+          SizedBox(
+            height: 10.h,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 15.w, right: 15.w),
+            child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
+                color: AppColors.txtFieldBackground,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      left: 5.w, right: 5.w, top: 15.h, bottom: 15.h),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 10.w,
+                          ),
+                          Text("#1"),
+                          SizedBox(
+                            width: 10.w,
+                          ),
+                          Text("Sample Action Name with long text"),
+                          SizedBox(
+                            width: 35.w,
+                          ),
+                          Icon(Icons.more_vert)
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ActionStatusWidget(title: ActionStatus.done),
+                          SizedBox(
+                            width: 10.w,
+                          ),
+                          Text("Due : Aug 15"),
+                        ],
+                      )
+                    ],
+                  ),
+                )),
+          )
         ],
+      ),
+    );
+  }
+}
+
+class ActionStatusWidget extends StatelessWidget {
+  final title;
+
+  const ActionStatusWidget({
+    Key? key,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 30.h,
+      width: 121.w,
+      decoration: BoxDecoration(
+          color: Color(0xFF6DE26B),
+          borderRadius: BorderRadius.all(Radius.circular(4.sp))),
+      child: Padding(
+        padding: EdgeInsets.all(8.sp),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Spacer(),
+              Icon(Icons.check, size: 16.sp, color: Color(0xFF393939)),
+              SizedBox(width: 2.sp),
+              Text(title,
+                  textAlign: TextAlign.center,
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 14.sp)),
+              Spacer()
+            ],
+          ),
+        ),
       ),
     );
   }
