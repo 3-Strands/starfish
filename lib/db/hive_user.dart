a0bbcd95789ea0717bbb915181919e55cc83f4be
Marcus Manvinder <marcus.manvinder@rubicotech.in>
2021-11-25 10:19:51 +0530

    *fix: '+' symbol is displayed if phone number is empty.

--- lib/db/hive_user.dart
+++ lib/db/hive_user.dart
@@ -151,7 +151,9 @@ class HiveUser extends HiveObject {
 
 extension HiveUserExt on HiveUser {
   String get phoneWithDialingCode {
-    return this.phone != null ? '+${this.diallingCode}${this.phone}' : '';
+    return this.phone != null && this.phone!.isNotEmpty
+        ? '+${this.diallingCode} ${this.phone}'
+        : '';
   }
 
   ActionStatus actionStatusbyId(HiveAction action) {
