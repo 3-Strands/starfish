57e8b4687299a6928b78385d274e45376c2f4455
Marcus Manvinder <marcus.manvinder@rubicotech.in>
2022-01-11 14:06:45 +0530

    *fix: missing inittialization of FileTransferClient.

--- lib/utils/services/grpc_client.dart
+++ lib/utils/services/grpc_client.dart
@@ -1,16 +1,21 @@
 import 'package:grpc/grpc_or_grpcweb.dart';
+import 'package:starfish/src/generated/file_transfer.pbgrpc.dart';
 import 'package:starfish/src/generated/starfish.pbgrpc.dart';
 import 'package:starfish/utils/services/local_storage_service.dart';
 
 class Singleton {
   static Singleton? _instance;
   StarfishClient? client;
+  FileTransferClient? fileTransferClient;
 
   Singleton._();
 
   static Singleton get instance => _instance ??= Singleton._();
 
-  Future initGprcClient() async {
+  final channel = GrpcOrGrpcWebClientChannel.toSingleEndpoint(
+      host: "sandbox-api.everylanguage.app", port: 443, transportSecure: true);
+
+  Future<Map<String, String>> initMetaData() async {
     String token = await StarfishSharedPreference().getAccessToken();
 
     Map<String, String>? metadata;
@@ -25,13 +30,20 @@ class Singleton {
         'x-api-key': 'AIzaSyCRxikcHzD0PrDAqG797MQyctEwBSIf5t0'
       };
     }
-    final channel = GrpcOrGrpcWebClientChannel.toSingleEndpoint(
-        host: "sandbox-api.everylanguage.app",
-        port: 443,
-        transportSecure: true);
+    return metadata;
+  }
+
+  Future initGprcClient() async {
     client = StarfishClient(
       channel,
-      options: CallOptions(metadata: metadata),
+      options: CallOptions(metadata: await initMetaData()),
+    );
+  }
+
+  Future initFileTransferClient() async {
+    fileTransferClient = FileTransferClient(
+      channel,
+      options: CallOptions(metadata: await initMetaData()),
     );
   }
 }
