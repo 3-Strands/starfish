a84b070bd46a49ff4d631de90e1024c96e026f87
Marcus Manvinder <marcus.manvinder@rubicotech.in>
2022-03-24 10:24:04 +0530

    integrate sentry to track error logs.

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -2,11 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:starfish/app.dart';
 import 'package:starfish/config/config_reader.dart';
 import 'package:starfish/utils/helpers/general_functions.dart';
 import 'config/app_config.dart';
 import 'db/hive_database.dart';
+
+const String _dsn =
+    'https://47b741149b0a493b9823c47085d69e1c@o1167952.ingest.sentry.io/6259436';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,7 +29,25 @@ void main() async {
         apiKey: ConfigReader.getProdAPIKey(),
       ),
     );
-    return runApp(Starfish());
+    //return runApp(Starfish());
+    return SentryFlutter.init(
+      (options) {
+        options.dsn = _dsn;
+        options.tracesSampleRate = 1.0;
+        options.reportPackages = false;
+        options.addInAppInclude('PRODUCTION');
+        options.considerInAppFramesByDefault = false;
+      },
+      // Init your App.
+      appRunner: () => runApp(
+        DefaultAssetBundle(
+          bundle: SentryAssetBundle(
+            enableStructuredDataTracing: true,
+          ),
+          child: Starfish(),
+        ),
+      ),
+    );
   });
   GeneralFunctions.configLoading();
 }
