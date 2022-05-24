3cb2f394be78d61d647629fd2e21cb28c3f7cc03
Hank Anish <hankanish@AdministorsAir9.lan>
2022-05-24 10:25:02 +0530

    Save transformation impact story and files to local for  result my life  and preview save files

--- lib/modules/results/my_life_results.dart
+++ lib/modules/results/my_life_results.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
+import 'package:dotted_border/dotted_border.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:focus_detector/focus_detector.dart';
 import 'package:intl/intl.dart';
 import 'package:starfish/bloc/app_bloc.dart';
@@ -10,13 +16,21 @@ import 'package:starfish/constants/assets_path.dart';
 import 'package:starfish/db/hive_current_user.dart';
 import 'package:starfish/db/hive_date.dart';
 import 'package:starfish/db/hive_evaluation_category.dart';
+import 'package:starfish/db/hive_file.dart';
 import 'package:starfish/db/hive_group.dart';
 import 'package:starfish/db/hive_group_user.dart';
 import 'package:starfish/db/hive_teacher_response.dart';
+import 'package:starfish/db/hive_transformation.dart';
 import 'package:starfish/db/providers/current_user_provider.dart';
 import 'package:starfish/db/providers/teacher_response_provider.dart';
+import 'package:starfish/db/providers/transformation_provider.dart';
+import 'package:starfish/modules/image_cropper/image_cropper_view.dart';
+import 'package:starfish/src/generated/file_transfer.pbgrpc.dart';
 import 'package:starfish/src/generated/starfish.pb.dart';
 import 'package:starfish/utils/date_time_utils.dart';
+import 'package:starfish/utils/helpers/uuid_generator.dart';
+import 'package:starfish/widgets/focusable_text_field.dart';
+import 'package:starfish/widgets/image_preview.dart';
 import 'package:starfish/widgets/month_year_picker/dialogs.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:starfish/widgets/result_transformations_widget.dart';
@@ -31,6 +45,10 @@ class MyLifeResults extends StatefulWidget {
 class _MyLifeResultsState extends State<MyLifeResults> {
   late AppBloc bloc;
   late HiveCurrentUser currentUser;
+  List<File>? imageFiles;
+  String? userImpactStory;
+
+  bool _isInitialized = false;
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +58,7 @@ class _MyLifeResultsState extends State<MyLifeResults> {
     if (bloc.resultsBloc.hiveGroup == null) {
       bloc.resultsBloc.init();
     }
+
     currentUser = CurrentUserProvider().getUserSync();
     return FocusDetector(
       onFocusGained: () {},
@@ -112,6 +131,7 @@ class _MyLifeResultsState extends State<MyLifeResults> {
                                   element.userId == currentUser.id);
 
                           return Container(
+                            key: UniqueKey(),
                             margin: EdgeInsets.only(left: 15.w, right: 15.w),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
@@ -135,15 +155,16 @@ class _MyLifeResultsState extends State<MyLifeResults> {
                                 SizedBox(
                                   height: 10.h,
                                 ),
-                                // ResultTransformationsWidget(
-                                //   groupUser: _hiveGroupUser!,
-                                //   month: bloc.resultsBloc.hiveDate!,
-                                // ),
+                                _buildTransformationWidget(
+                                    hiveGroupUser: _hiveGroupUser!,
+                                    learnerId: currentUser.id,
+                                    groupId: _hiveGroup.id!,
+                                    month: bloc.resultsBloc.hiveDate!),
                                 SizedBox(
                                   height: 10.h,
                                 ),
                                 _buildFeedbackFromTeachers(
-                                    hiveGroupUser: _hiveGroupUser!,
+                                    hiveGroupUser: _hiveGroupUser,
                                     learnerId: currentUser.id,
                                     groupId: _hiveGroup.id!,
                                     month: bloc.resultsBloc.hiveDate!),
@@ -658,6 +679,265 @@ class _MyLifeResultsState extends State<MyLifeResults> {
     );
   }
 
+  Card _buildTransformationWidget(
+      {required HiveGroupUser hiveGroupUser,
+      required String learnerId,
+      required String groupId,
+      required HiveDate month}) {
+    TextEditingController _transformationController = TextEditingController();
+    List<HiveFile> _hiveFiles = [];
+    List<File> _selectedFiles = [];
+
+    HiveTransformation? _currentGroupUserTransformation =
+        TransformationProvider()
+            .getGroupUserTransformations(learnerId, groupId)
+            .where((element) => element.month != null
+                ? (element.month!.year == month.year &&
+                    element.month!.month == month.month)
+                : false)
+            .firstOrNull;
+
+    if (_currentGroupUserTransformation != null &&
+        _currentGroupUserTransformation.impactStory != null) {
+      _transformationController.text =
+          _currentGroupUserTransformation.impactStory!;
+      print('this current $_currentGroupUserTransformation');
+      print('hive file ${_hiveFiles.length}');
+    }
+
+    if (_currentGroupUserTransformation != null &&
+        _currentGroupUserTransformation.localFiles.isNotEmpty) {
+      _hiveFiles = _currentGroupUserTransformation.localFiles;
+    }
+    return Card(
+      color: Color(0xE6EFEFEF),
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(
+            10,
+          ),
+        ),
+      ),
+      child: Container(
+        padding: EdgeInsets.only(left: 15.w, right: 15.w),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10.h,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  AssetsPath.resultsActiveIcon,
+                  color: Colors.green,
+                ),
+                SizedBox(
+                  width: 5.w,
+                ),
+                Text(
+                  '${AppLocalizations.of(context)!.transformations}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 19.sp,
+                    fontFamily: "OpenSans",
+                    color: Color(0xFF4F4F4F),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.h),
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xFFFFFFFF),
+                border: Border.all(
+                  color: Color(0xFF979797),
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: FocusableTextField(
+                controller: _transformationController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText:
+                      '${AppLocalizations.of(context)!.hintTextTransformationsTextField}',
+                  hintStyle: TextStyle(
+                    fontFamily: "OpenSans",
+                    fontSize: 16.sp,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                maxLines: 3,
+                textInputAction: TextInputAction.done,
+                onFocusChange: (isFocused) {
+                  if (isFocused) {
+                    return;
+                  }
+                },
+                onChange: (String value) {
+                  userImpactStory = value.trim();
+                  _saveTransformation(userImpactStory, _selectedFiles,
+                      hiveGroupUser, _currentGroupUserTransformation);
+                },
+              ),
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            //  if (_isEditMode) _previewFiles(widget.material!),
+            // SizedBox(height: 10.h),
+
+            // _previewSelectedFiles(),
+
+            // Add Materials
+
+            if (_selectedFiles.isNotEmpty ||
+                (_currentGroupUserTransformation != null &&
+                    _currentGroupUserTransformation.localFiles.isNotEmpty &&
+                    _hiveFiles != null &&
+                    _hiveFiles.isNotEmpty))
+              _previewSelectedFiles(_hiveFiles, _selectedFiles),
+            SizedBox(height: 10.h),
+            DottedBorder(
+              borderType: BorderType.RRect,
+              radius: Radius.circular(30.r),
+              color: Color(0xFF3475F0),
+              child: Container(
+                  width: double.infinity,
+                  height: 50.h,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if ((_selectedFiles.length + (_hiveFiles.length)) >= 5) {
+                        Fluttertoast.showToast(
+                            msg:
+                                AppLocalizations.of(context)!.maxFilesSelected);
+                      } else {
+                        FilePickerResult? result =
+                            await FilePicker.platform.pickFiles(
+                          allowMultiple: false,
+                          type: FileType.image,
+                          //  allowedExtensions: ['jpg', 'png', 'jpeg'],
+                        );
+
+                        if (result != null) {
+                          // if single selected file is IMAGE, open image in Cropper
+
+                          if (result.count == 1) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ImageCropperScreen(
+                                    sourceImage: File(result.paths.first!),
+                                    onDone: (File? _newFile) {
+                                      if (_newFile == null) {
+                                        return;
+                                      }
+                                      var fileSize = _newFile
+                                          .readAsBytesSync()
+                                          .lengthInBytes;
+                                      if (fileSize > 5 * 1024 * 1024) {
+                                        Fluttertoast.showToast(
+                                            msg: AppLocalizations.of(context)!
+                                                .imageSizeValidation);
+                                      } else {
+                                        setState(() {
+                                          _selectedFiles.add(_newFile);
+                                          _saveTransformation(
+                                              _transformationController.text,
+                                              _selectedFiles,
+                                              hiveGroupUser,
+                                              _currentGroupUserTransformation);
+                                          _selectedFiles.clear();
+                                        });
+                                      }
+                                    }),
+                              ),
+                            ).then((value) => {
+                                  // Handle cropped image here
+                                });
+                          } else {
+                            setState(() {
+                              _selectedFiles.addAll(result.paths
+                                  .map((path) => File(path!))
+                                  .toList());
+                            });
+                          }
+                        } else {
+                          // User canceled the picker
+                        }
+                      }
+                    },
+                    child: Text(
+                      '${AppLocalizations.of(context)!.addPhotos}',
+                      style: TextStyle(
+                        fontFamily: 'OpenSans',
+                        fontSize: 17.sp,
+                        color: Color(0xFF3475F0),
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                    ),
+                  )),
+            ),
+
+            SizedBox(
+              height: 20.h,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _saveTransformation(String? _impactStory, List<File> _files,
+      HiveGroupUser hiveGroupUser, HiveTransformation? _hiveTransformation) {
+    // _hiveTransformation = widget.hiveGroupUser
+    //     .getTransformationForMonth(bloc.resultsBloc.hiveDate!);
+    if (_impactStory != null) {
+      if (_hiveTransformation == null) {
+        _hiveTransformation = HiveTransformation();
+        _hiveTransformation.id = UuidGenerator.uuid();
+        _hiveTransformation.groupId = hiveGroupUser.groupId;
+        _hiveTransformation.userId = hiveGroupUser.userId;
+        _hiveTransformation.month = bloc.resultsBloc.hiveDate!;
+        _hiveTransformation.isNew = true;
+      } else {
+        _hiveTransformation.isUpdated = true;
+      }
+
+      _hiveTransformation.impactStory = _impactStory;
+    }
+
+    List<HiveFile> _transformationFiles = [];
+
+    if (_files.isNotEmpty) {
+      _files.forEach((_file) {
+        _transformationFiles.add(HiveFile(
+          entityId: _hiveTransformation!.id,
+          entityType: EntityType.TRANSFORMATION.value,
+          filepath: _file.path,
+          filename: _file.path.split("/").last,
+          isSynced: false,
+        ));
+      });
+    }
+
+    TransformationProvider()
+        .createUpdateTransformation(_hiveTransformation!,
+            transformationFiles: _transformationFiles)
+        .then((value) {
+      debugPrint("Transformation saved.");
+      // save files also
+    }).onError((error, stackTrace) {
+      debugPrint("Failed to save Transformation");
+    });
+  }
+
   Future<DateTime?> _selectMonth(AppBloc bloc) async {
     // reference for the MonthYearPickerLocalizations is add in app.dart
     return await showMonthYearPicker(
@@ -669,6 +949,98 @@ class _MyLifeResultsState extends State<MyLifeResults> {
       firstDate: DateTime(2011),
       lastDate: DateTime.now(),
       hideActions: true,
+    );
+  }
+
+  Widget _previewSelectedFiles(
+      List<HiveFile> _hiveFiles, List<File> _selectedFiles) {
+    final List<Widget> _widgetList = [];
+
+    for (File file in _selectedFiles) {
+      _widgetList.add(_imagePreview(
+          file: file,
+          onDelete: () {
+            setState(() {
+              _selectedFiles.remove(file);
+            });
+          }));
+    }
+
+    if (_hiveFiles.isNotEmpty) {
+      for (HiveFile _hiveFile in _hiveFiles) {
+        File file = File(_hiveFile.filepath!);
+        _widgetList.add(_imagePreview(
+            file: file,
+            onDelete: () {
+              setState(() {
+                _hiveFile.delete();
+              });
+            }));
+      }
+    }
+
+    return GridView.count(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      crossAxisCount: (_selectedFiles.length == 1 && _hiveFiles.length == 0) ||
+              (_hiveFiles.length == 1 && _selectedFiles.length == 0)
+          ? 1
+          : 2,
+      childAspectRatio: 1,
+      children: _widgetList,
+    );
+  }
+
+  Widget _imagePreview({required File file, required Function onDelete}) {
+    return Stack(
+      alignment: AlignmentDirectional.topEnd,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.only(top: 10.0, right: 10.0),
+          child: Container(
+            child: InkWell(
+              onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => ImagePreview(file))),
+              child: Hero(
+                tag: file,
+                child: Card(
+                  margin: const EdgeInsets.only(top: 12.0, right: 12.0),
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.file(
+                      file,
+                      fit: BoxFit.scaleDown,
+                      //  height: 130.h,
+                    ),
+                  ),
+                ),
+                flightShuttleBuilder: (flightContext, animation, direction,
+                    fromContext, toContext) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            // setState(() {
+            //   _selectedFiles.remove(file);
+            // });
+            onDelete();
+          },
+          icon: Icon(
+            Icons.delete,
+            color: Colors.red,
+          ),
+        ),
+      ],
     );
   }
 }
