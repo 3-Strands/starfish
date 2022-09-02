7119b8d321b773f0365f3edd91cb792cd6e33704
Marcus Manvinder <marcus.manvinder@rubicotech.in>
2022-09-02 10:01:26 +0530

    Use UpdateTransformationDelta for transformation update.

--- lib/modules/results/cubit/add_edit_transformation_cubit.dart
+++ lib/modules/results/cubit/add_edit_transformation_cubit.dart
@@ -5,9 +5,9 @@ import 'package:meta/meta.dart';
 import 'package:starfish/models/file_reference.dart';
 import 'package:starfish/repositories/data_repository.dart';
 import 'package:starfish/src/deltas.dart';
-import 'package:starfish/src/generated/file_transfer.pbgrpc.dart';
-import 'package:starfish/src/generated/google/type/date.pb.dart';
+import 'package:starfish/src/generated/file_transfer.pbenum.dart';
 import 'package:starfish/src/grpc_extensions.dart';
+import 'package:starfish/utils/helpers/uuid_generator.dart';
 import 'package:starfish/wrappers/file_system.dart';
 
 part 'add_edit_transformation_state.dart';
@@ -20,6 +20,7 @@ class AddEditTransformaitonCubit extends Cubit<AddEditTransformationState> {
     Transformation? transformation,
   })  : _transformation = transformation,
         _dataRepository = dataRepository,
+        _id = transformation?.id ?? UuidGenerator.uuid(),
         super(AddEditTransformationState(
           groupUser: groupUser,
           month: month,
@@ -34,16 +35,17 @@ class AddEditTransformaitonCubit extends Cubit<AddEditTransformationState> {
 
   final DataRepository _dataRepository;
   final Transformation? _transformation;
+  final String _id;
 
   void addFile(File file) {
     emit(state
         .copyWith(newlySelectedFiles: [...state.newlySelectedFiles, file]));
 
-    // _dataRepository.addDelta(FileReferenceCreateDelta(
-    //     entityId: _transformation?.id,
-    //     entityType: EntityType.TRANSFORMATION,
-    //     filename: file.name,
-    //     filepath: file.path));
+    _dataRepository.addDelta(FileReferenceCreateDelta(
+        entityId: _id,
+        entityType: EntityType.TRANSFORMATION,
+        filename: file.path.split("/").last,
+        filepath: file.path));
   }
 
   void removeFile(File file) {
@@ -56,12 +58,16 @@ class AddEditTransformaitonCubit extends Cubit<AddEditTransformationState> {
   }
 
   void saveImpactStory() {
-    _dataRepository.addDelta(TransformationCreateDelta(
-      userId: state.groupUser.userId,
-      groupId: state.groupUser.groupId,
-      month: state.month,
-      impactStory: state.impactStory,
-    ));
+    final transformation = _transformation;
+    _dataRepository.addDelta(transformation == null
+        ? TransformationCreateDelta(
+            userId: state.groupUser.userId,
+            groupId: state.groupUser.groupId,
+            month: state.month,
+            impactStory: state.impactStory,
+          )
+        : TransformationUpdateDelta(transformation,
+            impactStory: state.impactStory));
   }
 
   @override
